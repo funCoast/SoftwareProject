f76b3f7b5e3563468506cbf72bd726afb576296e
冉啟何 <22373028@buaa.edu.cn>
2025-04-11 21:19:06 +0800

    feat: 实现将时间戳转化为时间的插件

---
+++ backEnd/DjangoProject/lxc/api/core/plugin/plugins/time_plugin/timestamp_transform_plugin.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+import pytz
+
+from api.core.plugin.plugins.base_plugin import BasePlugin
+
+
+class TimestampTransformPlugin(BasePlugin):
+    def __init__(self):
+        super().__init__(
+            name="TimestampTransformPlugin",
+            version="1.0",
+            description="时间戳转换为时间"
+        )
+
+    def execute(self, *args, **kwargs):
+        # 从 kwargs 中获取时区信息、时间戳
+        timezone = kwargs.get("timezone")
+        timestamp = kwargs.get("timestamp")
+        try:
+            if timezone:
+                tz = pytz.timezone(timezone)
+            else:
+                tz = datetime.now().astimezone().tzinfo
+
+            utc_time = datetime.fromtimestamp(int(timestamp), tz)
+            local_time = utc_time.astimezone(tz)
+
+            timestamp_info = {
+                "time_str": local_time.strftime("%Y-%m-%d %H:%M:%S"),
+            }
+            return {"status": "success", "result": timestamp_info}
+        except pytz.UnknownTimeZoneError:
+            return {"status": "error", "message": f"Unknown timezone: {timezone}"}
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
+if __name__ == "__main__":
+    print(TimestampTransformPlugin().execute(timezone="Asia/Shanghai", timestamp="11"))
