d7be84a9397e5f99fe07ff18b8ecb4a37dacdfc6
冉啟何 <22373028@buaa.edu.cn>
2025-04-13 10:26:08 +0800

    feat: 注册现有插件

--- backEnd/DjangoProject/lxc/api/core/plugin/api/plugin_api.py
+++ backEnd/DjangoProject/lxc/api/core/plugin/api/plugin_api.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, jsonify
 
 from api.core.plugin.managers.plugin_manager import PluginManager
-
-# from managers.plugin_manager import PluginManager
+from api.core.plugin.plugins.audio.Speech2TextPlugin import SpeechToTextPlugin
+from api.core.plugin.plugins.code.code_run_plugin import CodeRunPlugin
+from api.core.plugin.plugins.time.current_time_plugin import CurrentTimePlugin
+from api.core.plugin.plugins.time.timestamp_pulgin import TimestampPlugin
+from api.core.plugin.plugins.time.timestamp_transform_plugin import TimestampTransformPlugin
+from api.core.plugin.plugins.time.timezoen_switch_plugin import TimezoneSwitchPlugin
+from api.core.plugin.plugins.time.weekday_calculator_plugin import WeekdayCalculatorPlugin
+from api.core.plugin.plugins.weather.WeatherForecastPlugin import WeatherScraperPlugin
 
 app = Flask(__name__)
 plugin_manager = PluginManager()
 
-# TODO 注册插件
-# plugin_manager.register_plugin(TimePlugin())
-# plugin_manager.register_plugin(WeatherPlugin())
-# plugin_manager.register_plugin(WebScraperPlugin())
-# plugin_manager.register_plugin(SpeechToTextPlugin())
-# plugin_manager.register_plugin(TextToSpeechPlugin())
-# plugin_manager.register_plugin(CodeInterpreterPlugin())
+plugin_manager.register_plugin(CurrentTimePlugin)
+plugin_manager.register_plugin(TimestampPlugin)
+plugin_manager.register_plugin(TimestampTransformPlugin)
+plugin_manager.register_plugin(TimezoneSwitchPlugin)
+plugin_manager.register_plugin(WeekdayCalculatorPlugin)
+plugin_manager.register_plugin(CodeRunPlugin)
+plugin_manager.register_plugin(SpeechToTextPlugin)
+plugin_manager.register_plugin(WeatherScraperPlugin)
 
 @app.route('/plugins', methods=['GET'])
 def list_plugins():
