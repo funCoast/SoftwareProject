244e516f4d37fd757d1fd543979e1962dd419b95
冉啟何 <22373028@buaa.edu.cn>
2025-04-14 01:18:58 +0800

    chore: 删除不必要的文件

--- backEnd/DjangoProject/lxc/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from api.core.plugin.api import register_plugins
-from api.core.plugin.managers.plugin_manager import PluginManager
-
-plugin_manager = PluginManager()
-register_plugins(plugin_manager)
--- backEnd/DjangoProject/lxc/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from api.core.plugin.api.views import list_plugins, execute_plugin
-
-urlpatterns = [
-    path('plugins/', list_plugins, name='list_plugins'),
-    path('plugins/<str:plugin_name>/execute/', execute_plugin, name='execute_plugin'),
-]
