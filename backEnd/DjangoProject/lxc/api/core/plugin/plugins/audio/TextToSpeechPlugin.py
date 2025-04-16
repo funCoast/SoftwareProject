f093f4e0b6fb21e509a08f08bc197ddfdc0b28a7
冉啟何 <22373028@buaa.edu.cn>
2025-04-16 16:56:15 +0800

    feat: 实现文字转语音插件

---
+++ backEnd/DjangoProject/lxc/api/core/plugin/plugins/audio/TextToSpeechPlugin.py
@@ -0,0 +1,57 @@
+import time
+
+from gtts import gTTS
+from pydub import AudioSegment
+from pymysql.converters import convert_time
+
+from api.core.plugin.plugins.base_plugin import BasePlugin
+
+
+def convert_to_wav(input_file, output_file):
+    audio = AudioSegment.from_mp3(input_file)
+    audio.export(output_file, format="wav")
+
+
+class TextToSpeechPlugin(BasePlugin):
+    def __init__(self):
+        self.name = "TextToSpeech"
+        self.version = "1.0"
+        self.description = "将文本转化为语音"
+
+    def execute(self, *args, **kwargs):
+        try:
+            text = kwargs.get('text')
+            language = kwargs.get('language')
+
+            if text is None:
+                return {
+                    "success": "error",
+                    "message": "没有文本输入"
+                }
+
+            if language is None:
+                language = "zh-cn"
+
+            tts = gTTS(text=text, lang=language)
+
+            tts.save(f"mp3-{time.strftime("%Y%m%d_%H%M%S")}-audio.mp3")
+
+            output_path = "output.wav"
+            convert_to_wav(f"mp3-{time.strftime("%Y%m%d_%H%M%S")}-audio.mp3", output_path)
+
+            return {
+                "status": "success",
+                "result": {
+                    "speech_path": output_path
+                }
+            }
+
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
+
+
+if __name__ == '__main__':
+    TextToSpeechPlugin().execute(text="原神，启动！", language="zh-cn")
