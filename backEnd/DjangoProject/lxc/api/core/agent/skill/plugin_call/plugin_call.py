887d7eff82c13b3ea447fbcb8b17cc1f8bc1da08
冉啟何 <22373028@buaa.edu.cn>
2025-04-20 11:10:28 +0800

    fix: 修改插件自调用方法

--- backEnd/DjangoProject/lxc/api/core/agent/skill/plugin_call/plugin_call.py
+++ backEnd/DjangoProject/lxc/api/core/agent/skill/plugin_call/plugin_call.py
@@ -11,30 +11,24 @@ def execute(plugin: BasePlugin):
     return plugin.execute()
 
 
-class SkillPluginCall:
+def choose_and_run(input_text: str):
+    text = translate_to_english(input_text)
+    print("英文文本：", text)
 
-    def __init__(self):
-        self.plugins = []
+    # 意图识别
+    intent = intent_recognition(text, list(plugin_manager.intent_dict.keys()))
+    print("意图识别结果：", intent.split(" "))
+    intents = intent.split(" ")
 
-    def add_plugin(self, plugin: BasePlugin):
-        self.plugins.append(plugin)
+    for intent in intents:
+        kwargs = json.loads(extract_parameters_by_model(text, tools[intent]))
+        print(kwargs)
+        print(plugin_manager.intent_dict[intent]().execute(**kwargs))
 
-    def choose_and_run(self, input_text: str):
-        text = translate_to_english(input_text)
-        print("英文文本：", text)
 
-        # 意图识别
-        intent = intent_recognition(text, list(plugin_manager.intent_dict.keys()))
-        print("意图识别结果：", intent.split(" "))
-        intents = intent.split(" ")
-
-        for intent in intents:
-            kwargs = json.loads(extract_parameters_by_model(text, tools[intent]))
-            print(kwargs)
-            print(plugin_manager.intent_dict[intent]().execute(**kwargs))
 
 if __name__ == '__main__':
-    SkillPluginCall().choose_and_run("北京现在几点")
+    choose_and_run("北京现在几点")
 
 
 
