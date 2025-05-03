b85ff14a1b4fdd91258b747cc9915efb63164587
funCoast <112672203+funCoast@users.noreply.github.com>
2025-05-03 23:49:34 +0800

    delete autherInAgent

--- backEnd/DjangoProject/lxc/backend/models.py
+++ backEnd/DjangoProject/lxc/backend/models.py
@@ -195,7 +195,6 @@ class Agent(models.Model):
     icon_url = models.URLField(max_length=500, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='agents')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='private')
-    author = models.ForeignKey('User', on_delete=models.CASCADE, related_name='agents')
 
     def __str__(self):
         return self.agent_name
