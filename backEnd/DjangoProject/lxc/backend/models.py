f6d70b0e44f838ed132da5cc17c4cb8425dc442f
冉啟何 <22373028@buaa.edu.cn>
2025-05-04 14:46:36 +0800

    fix(agent): 在数据模型中添加发布时间

--- backEnd/DjangoProject/lxc/backend/models.py
+++ backEnd/DjangoProject/lxc/backend/models.py
@@ -194,6 +194,7 @@ class Agent(models.Model):
     icon_url = models.URLField(max_length=500, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='agents')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='private')
+    publish_time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.agent_name
